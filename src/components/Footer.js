f86539e85eff20e9a33f2a3834bd0106b0df90db
jokojie24 <108077911+jokojie24@users.noreply.github.com>
2024-07-13 02:23:53 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -65,7 +65,7 @@ import {
             spacing={4}
             justify={{ base: 'center', md: 'space-between' }}
             align={{ base: 'center', md: 'center' }}>
-            <Text>O.FX   (c) 2022</Text>
+            <Text>O.FX   &copy 2022</Text>
             <Stack direction={'row'} spacing={6}>
               <SocialButton label={'Twitter'} href={'https://twitter.com/The_prof1'}>
                 <FaTwitter />
